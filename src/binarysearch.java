11d21c1861817ae6818d4afd43ea8eb60d74e6d0
RISHIKESHdev <rishikeshhappydeveloper@gmail.com>
2023-08-02 22:46:11 +0530

    Day 5

--- src/binarysearch.java
+++ src/binarysearch.java
@@ -23,6 +23,9 @@ public class binarysearch {
             case 4: // count negative in a 2D matrix
                 System.out.println(countNegatives(input.get2dNumArr(in)));
                 break;
+            case 5: // arrange coins
+                System.out.println(arrangeCoins(input.getNum(in)));
+                break;
         }
         in.close();
     }
@@ -101,5 +104,22 @@ public class binarysearch {
             m=-1;
         }
         return res;
+    }
+    public  static int arrangeCoins(int n){
+        long left = 0, right = n;
+    long k, curr;
+    while (left <= right) {
+      k = left + (right - left) / 2;
+      curr = k * (k + 1) / 2;
+
+      if (curr == n) return (int)k;
+
+      if (n < curr) {
+        right = k - 1;
+      } else {
+        left = k + 1;
+      }
+    }
+    return (int)right;
     }
 }

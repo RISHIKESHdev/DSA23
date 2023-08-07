7b5a4c0b4e8811a6d628a652b2c3d157068e32a7
RISHIKESHdev <rishikeshhappydeveloper@gmail.com>
2023-08-07 23:37:54 +0530

    Day 2

--- src/binarysearch.java
+++ src/binarysearch.java
@@ -2,6 +2,7 @@ package src;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
 
 import common.*;
@@ -63,6 +64,12 @@ public class binarysearch {
             case 17 : //Aggressive Cows
                 System.out.println(aggressiveCows(input.getNumArr(in),input.getNum(in)));
                 break;
+            case 18 : //Allocate minimum number of pages
+                System.out.println(findPages(input.getNumArrAsen(in),input.getNum(in),input.getNum(in)));
+                break;
+            case 19 : //Allocate minimum number of pages
+                System.out.println(maxJump(input.getNumArrAsen(in)));
+                break;
         }
         in.close();
     }
@@ -103,7 +110,7 @@ public class binarysearch {
         int nele=idx+1;
         return nele;
     }
-    public static boolean isPossible(int[] nums,int mid,int k){
+    public static boolean isPossibleMinCapability(int[] nums,int mid,int k){
         int lastStolenIndex = -2;
         for(int i=0;i<nums.length;i++){
             if(nums[i]<=mid && lastStolenIndex + 1 < i){
@@ -114,7 +121,7 @@ public class binarysearch {
         }
         return false;
     }
-    public static boolean isPossible(int a[], int n, int cows, int minDist) {
+    public static boolean isPossibleCows(int a[], int n, int cows, int minDist) {
         int count = 1;
         int prev = a[0];
         for (int i = 1; i < n; i++) {
@@ -125,6 +132,43 @@ public class binarysearch {
         }
         if (count >= cows) return true;
         return false;
+    }
+    public static boolean isPossiblePage(int[] nums,int n,int m,int mid){
+        int stuCount=0,cursum=0;
+        for(int i=0;i<n;i++){
+            cursum=nums[i];
+            if(cursum>mid){
+                stuCount++;
+                cursum=nums[i];
+            }
+        }
+        return stuCount<=m;
+    }
+    public static boolean isPossibleFrogJump(int[] stones, int maxJump){
+        int n=stones.length;
+        HashSet<Integer> visited = new HashSet<>();
+        for(int i=0;i<n;i++){
+            if(i==n-1) break;
+            else if(i+1==n-1){
+                if(i!=0) visited.add(i);
+                break;
+            }
+            else if(stones[i+2]-stones[i] <= maxJump){
+                if(i != 0) visited.add(i);
+            }
+             else return false;
+        }
+        for(int i=n-1;i>=0;i--){
+            if(i==0) break;
+            else if(i-1==0){
+                if(stones[i]-stones[i-1] <= maxJump) break;
+                else return false;
+            }
+            else if( visited.contains(i-1) ){
+                if (!(stones[i]-stones[i-2] <= maxJump)) return false; 
+            }
+        }
+        return true;
     }
     /////////////////////////////////////////////////////////////////////////////////////
     /////////////////////////////////////////////////////////////////////////////////////
@@ -431,7 +475,7 @@ public static int findMiddleOccurceArr(int[]nums,int target)
         int l=min,r=max,m=-1;
         while(l<r){
             m=(l+r)/2;
-            if(isPossible(nums,m,k)){
+            if(isPossibleMinCapability(nums,m,k)){
                 r=m;
             }else{
                 l=m+1;
@@ -445,9 +489,41 @@ public static int findMiddleOccurceArr(int[]nums,int target)
         int l=1,r=stalls[n-1]-stalls[0],m=-1;
         while(l<=r){
             m=(l+r)/2;
-            if(isPossible(stalls,n,k,m)) l=m+1;
+            if(isPossibleCows(stalls,n,k,m)) l=m+1;
             else r=m-1;
         }
         return r;
+    }
+    public static int findPages(int[] nums,int n,int m){
+        int sum=0,res=0;
+        if(n<m) return -1;
+        for(int i=0;i<n;i++) sum=sum+nums[i];
+        int l=nums[n-1],r=sum,mid=0;
+        while(l<=r){
+            mid=(l+r)/2;
+            if(isPossiblePage(nums,n,m,mid)){
+                res=mid;
+                r=mid-1;
+            }else l=mid+1;
+        }
+        return res;
+    }
+    public static int maxJump(int[] stones) {
+        int n= stones.length;
+        int left= 1;
+        int right= stones[n-1]-stones[0];
+        int minJump= right;
+        while(left <= right){
+            int mid= (left+right)/2;
+            boolean possible= isPossibleFrogJump(stones, mid);
+            if(possible){
+                minJump= mid;
+                right= mid-1;                
+            }
+            else{
+                left= mid+1;
+            }
+        }
+        return minJump;
     }
 }
